8a697c47d920ad50d1d5b1045dcd20c808fb82cf
naem1023 <relilau00@gmail.com>
2021-05-20 18:52:05 +0900

    Switch to own model - Make MIS model - Fix training method

--- ny/ny.py
+++ ny/ny.py
@@ -31,33 +31,45 @@ def get_ny_data():
 class NyDataset(Dataset):
     """Newyork Data"""
 
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, transform=None, x_point=10, y_point=10):
         """
         Args:
-            root_dir (string): 모든 이미지가 존재하는 디렉토리 경로
-            transform (callable, optional): 샘플에 적용될 Optional transform
+            root_dir (string):
+                모든 이미지가 존재하는 디렉토리 경로
+            transform (callable, optional):
+                샘플에 적용될 Optional transform
+            point (int):
+                이미즈 한 변의 point 개수
         """
         self.root_dir = root_dir
         self.img_data_file = h5py.File(root_dir)
         self.transform = transform
+        self.x_point = x_point
+        self.y_point = y_point
+        self.point = x_point * y_point
 
         f = h5py.File(self.root_dir)
-        print('test', f['images'].shape)
-        print('test', f['rawDepths'].shape)
-        print('test', f['depths'].shape)
 
         self.len = f['images'].shape[0]
 
     def __len__(self):
-        return self.len
+        return self.len * self.point
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        image = self.__get_image(self.root_dir, idx)
-        raw_depth_image = self.__get_raw_depth(self.root_dir, idx)
-        depth_image = self.__get_depth(self.root_dir, idx)
+        if type(idx) is list:
+            converted_idx = np.array([(i // self.point, i % self.point) for i in idx])
+        elif type(idx) is int:
+            converted_idx = np.array([[idx // self.point, idx % self.point]])
+            # converted_idx = np.reshape(converted_idx, (converted_idx.shape[0], 1))
+
+        image = self.__get_image(self.root_dir, converted_idx[:, 0])
+        # raw_depth_image = self.__get_raw_depth(self.root_dir, converted_idx[:, 0])
+        depth_image = self.__get_depth(self.root_dir, converted_idx[:, 0])
+
+        depth_list = self.get_depth_point(depth_image, converted_idx[:, 1])
 
         # sample = {
         #     'image': image,
@@ -68,63 +80,72 @@ class NyDataset(Dataset):
         # if self.transform:
         #     sample = self.transform(sample)
 
-        return image, depth_image
+        return image, depth_list
+
+    def get_depth_point(self, depth_image, idxes):
+        # Not coordinate of image, only order of training points.
+        positions = [ [idx % self.point // self.x_point, idx % self.point % self.x_point ] for idx in idxes]
+        x_interval = depth_image.shape[1] // self.x_point
+        y_interval = depth_image.shape[2] // self.y_point
+
+        depth = [ depth_image[0][pos[0] * x_interval][pos[1] * y_interval] for pos in positions ]
+        # Range of depth is 0 to 10. So divide by 10.
+        depth = np.array(depth) * 1000
+        depth = depth.astype(np.int64)
+
+        return depth
 
     def __get_raw_depth(self, root_dir, idx):
-        rawDepth = self.img_data_file['rawDepths'][idx]
-
+        rawDepth = self.img_data_file['rawDepths'][idx] / 4.0
         # return rawDepth
-        rawDepth_ = np.empty([480, 640, 3])
-        rawDepth_[:, :, 0] = rawDepth[:, :].T
-        rawDepth_[:, :, 1] = rawDepth[:, :].T
-        rawDepth_[:, :, 2] = rawDepth[:, :].T
+        # rawDepth_ = np.empty([480, 640, 3])
+        # rawDepth_[:, :, 0] = rawDepth[:, :].T
+        # rawDepth_[:, :, 1] = rawDepth[:, :].T
+        # rawDepth_[:, :, 2] = rawDepth[:, :].T
 
         # image = io.imread(rawDepth_ / 4.0)
         return rawDepth
 
     def __get_depth(self, root_dir, idx):
-        depth = self.img_data_file['depths'][idx]
+        depth = self.img_data_file['depths'][idx] # (1, 640, 480)
         # return depth
-        depth_ = np.empty([480, 640, 3])
-        depth_[:, :, 0] = depth[:, :].T
-        depth_[:, :, 1] = depth[:, :].T
-        depth_[:, :, 2] = depth[:, :].T
+        # depth_ = np.empty([480, 640, 1])
+        # depth_[:, :, 0] = depth[:, :].T
+        # depth_[:, :, 1] = depth[:, :].T
+        # depth_[:, :, 2] = depth[:, :].T
+        # depth_ = depth.T
 
+        transform_depth = depth.astype('float32') / 4.0
         # image = io.imread(depth_ / 4.0)
-        return depth
+        return transform_depth
 
     def __get_image(self, root_dir, idx):
-        img = self.img_data_file['images'][idx]
+        img = self.img_data_file['images'][idx][0] # (3, 640, 480)
         # return img
-        img_ = np.empty([480, 640, 3])
-        img_[:, :, 0] = img[0, :, :].T
-        img_[:, :, 1] = img[1, :, :].T
-        img_[:, :, 2] = img[2, :, :].T
+        # img_ = np.empty([480, 640, 3])
+        # img_[:, :, 0] = img[0, :, :].T
+        # img_[:, :, 1] = img[1, :, :].T
+        # img_[:, :, 2] = img[2, :, :].T
 
-        imag_ = img_.astype('float64')
-        img = img.astype('float32')
+        transform_img = img.astype('float32') / 255.0
+        # img = img.astype('float32') / 255.0
         # image = io.imread(imag_ / 255.0)
-        return img
+        return transform_img
 
 
 # v1의 파일이름 배열 부르기
 file_name = scipy.io.loadmat(path_to_filename_v1)
 dname = file_name['rawDepthFilenames']
-# print(dname)
 
 rname = file_name['rawRgbFilenames']
-# print(rname)
 
 # 첫번째 rawDepths, 480*640
 rawDepth = f['rawDepths'][0]
-# print(f['rawDepths'].size)
-print(f['rawDepths'])
 # 전치
 rawDepth_=np.empty([480,640,3])
 rawDepth_[:,:,0]=rawDepth[:,:].T
 rawDepth_[:,:,1]=rawDepth[:,:].T
 rawDepth_[:,:,2]=rawDepth[:,:].T
-print('rawDepth', type(rawDepth[0][0]))
 #
 # io.imshow(rawDepth_/4.0)
 # io.show()
@@ -139,7 +160,6 @@ img_[:,:,1]=img[1,:,:].T
 img_[:,:,2]=img[2,:,:].T
 
 imag_=img_.astype('float32')
-print('img', type(img[0][0][0]))
 io.imshow(imag_/255.0)
 # io.show()
 
@@ -153,6 +173,26 @@ depth_[:,:,0]=depth[:,:].T
 depth_[:,:,1]=depth[:,:].T
 depth_[:,:,2]=depth[:,:].T
 print('depth', type(depth[0][0]))
+
 #
 # io.imshow(depth_/4.0)
 # io.show()
+
+from tqdm import tqdm
+
+def check_minmax():
+    max_val = []
+    min_val = []
+    for origin_depth in tqdm(f['depths']):
+        max_val.append(np.max(origin_depth))
+        min_val.append(np.min(origin_depth))
+        # depth_ = np.empty([480, 640, 1])
+        # depth_[:, :, 0] = origin_depth[:, :].T
+        # depth_[:, :, 1] = origin_depth[:, :].T
+        # depth_[:, :, 2] = origin_depth[:, :].T
+        #
+        # print('depth', type(origin_depth[0][0]))
+
+    print(max_val, min_val)
+
+    # #전치
--- ny/train.py
+++ ny/train.py
@@ -1,8 +1,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import ny
-import resnet
+import ny.ml_model
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
@@ -20,88 +19,79 @@ transform = transforms.Compose([
     # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
-def get_dataset(path):
-    ny_dataset = ny.NyDataset(path)
-    # print(len(ny_dataset))
-    # dataset = torch.utils.data.DataLoader(dataset=ny_dataset, batch_size=64, shuffle=True)
 
-    # Set split lenght
-    train_ratio = 0.8
-    train_len = int(len(ny_dataset) * train_ratio)
-    val_len = len(ny_dataset) - train_len
+class Train:
+    def __init__(self):
+        pass
 
-    train_dataset, val_dataset = torch.utils.data.random_split(ny_dataset, [train_len, val_len])
+    def train(self, data):
+        train_loader, test_loader = data
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # torch.manual_seed(53)
+        # if device == 'cuda':
+        #     torch.cuda.manual_seed_all(53)
 
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=32, shuffle=True)
+        model = ny.ml_model.MIS()
+        faster_rcnn_feature_extractor = model.feature_extraction(device)
 
-    return train_loader, val_loader
+        output_map = faster_rcnn_feature_extractor()
 
-def train(data):
-    use_cuda = torch.cuda.is_available()
-    if use_cuda:
-        print('you can use cuda')
-    train_loader, test_loader = data
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # torch.manual_seed(53)
-    # if device == 'cuda':
-    #     torch.cuda.manual_seed_all(53)
+        model = model.to(device)
 
-    model = resnet.get_model()
-    model = model.to(device)
+        # Optimize
+        # criterion = nn.CrossEntropyLoss().cuda()
+        criterion = nn.SmoothL1Loss().cuda()
+        # optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
+        optimizer = optim.Adam(model.parameters(), lr=0.01)
 
-    # Optimize
-    criterion = nn.CrossEntropyLoss().cuda()
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
+        import time
+        start_time = time.time()
+        min_loss = int(1e9)
+        history = {'loss': [], 'val_acc': []}
+        for epoch in range(1):  # loop over the dataset multiple times
+            epoch_loss = 0.0
+            tk0 = tqdm(train_loader, total=len(train_loader), leave=False)
+            for step, (inputs, labels) in enumerate(tk0, 0):
+                inputs, labels = inputs.to(device), labels.to(device)
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-    import time
-    start_time = time.time()
-    min_loss = int(1e9)
-    history = {'loss': [], 'val_acc': []}
-    for epoch in range(1):  # loop over the dataset multiple times
-        epoch_loss = 0.0
-        tk0 = tqdm(train_loader, total=len(train_loader), leave=False)
-        for step, (inputs, labels) in enumerate(tk0, 0):
-            inputs, labels = inputs.to(device), labels.to(device)
-            # zero the parameter gradients
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+                epoch_loss += loss.item()
 
-            epoch_loss += loss.item()
+            # validation
+            if epoch % 10 == 0:
+                class_correct = list(0. for i in range(1000))
+                class_total = list(0. for i in range(1000))
+                with torch.no_grad():
+                    for data in test_loader:
+                        images, labels = data
+                        images = images.cuda()
+                        labels = labels.cuda()
+                        outputs = model(images)
+                        _, predicted = torch.max(outputs, 1)
+                        c = (predicted == labels).squeeze()
+                        for i in range(labels.size()[0]):
+                            label = labels[i].item()
+                            class_correct[label] += c[i].item()
+                            class_total[label] += 1
+                val_acc = sum(class_correct) / sum(class_total) * 100
+            else:
+                val_acc = 0
 
-        # validation
-        if epoch % 10 == 0:
-            class_correct = list(0. for i in range(1000))
-            class_total = list(0. for i in range(1000))
-            with torch.no_grad():
-                for data in test_loader:
-                    images, labels = data
-                    images = images.cuda()
-                    labels = labels.cuda()
-                    outputs = model(images)
-                    _, predicted = torch.max(outputs, 1)
-                    c = (predicted == labels).squeeze()
-                    for i in range(labels.size()[0]):
-                        label = labels[i].item()
-                        class_correct[label] += c[i].item()
-                        class_total[label] += 1
-            val_acc = sum(class_correct) / sum(class_total) * 100
-        else:
-            val_acc = 0
+            # print statistics
+            tqdm.write('[Epoch : %d] train_loss: %.5f val_acc: %.2f Total_elapsed_time: %d 분' %
+                       (epoch + 1, epoch_loss / 272, val_acc, (time.time() - start_time) / 60))
+            history['loss'].append(epoch_loss / 272)
+            history['val_acc'].append(val_acc)
 
-        # print statistics
-        tqdm.write('[Epoch : %d] train_loss: %.5f val_acc: %.2f Total_elapsed_time: %d 분' %
-                   (epoch + 1, epoch_loss / 272, val_acc, (time.time() - start_time) / 60))
-        history['loss'].append(epoch_loss / 272)
-        history['val_acc'].append(val_acc)
+            if epoch in [36, 64, 92]:
+                for g in optimizer.param_groups:
+                    g['lr'] /= 10
+                print('Loss 1/10')
 
-        if epoch in [36, 64, 92]:
-            for g in optimizer.param_groups:
-                g['lr'] /= 10
-            print('Loss 1/10')
-
-    print(time.time() - start_time)
-    print('Finished Training')
+        print(time.time() - start_time)
+        print('Finished Training')
